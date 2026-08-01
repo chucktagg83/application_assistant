e5a42e62304f22653c58ae9cc1b96e4dc02e3c29
Chuck Tagg <deckplatelegacy@gmail.com>
2026-08-01 06:37:57 -0600

    Added in Login/out

--- applications/urls.py
+++ applications/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 
 from .views import home_page_view
+from . import views 
+
+app_name = 'applications'
 
 urlpatterns = [
-    path('', home_page_view),
+    path('', home_page_view, name='home'),
+    path('home/', views.home_page_view, name='home')
 ]
--- applications/views.py
+++ applications/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home_page_view(request):
-    return HttpResponse("Hello, World!")
+    return render(request, 'applications/home.html')
