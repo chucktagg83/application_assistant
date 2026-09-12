d8db57dea30fa069b5d5278739b0b2e15aefa2cb
Chuck Tagg <deckplatelegacy@gmail.com>
2026-09-11 19:16:49 -0600

    Configure static files for Render

--- application_assistant/settings.py
+++ application_assistant/settings.py
@@ -126,8 +126,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static',]
+# ==============================
+# STATIC FILES
+# ==============================
+
+STATIC_URL = "/static/"
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'applications:home'
